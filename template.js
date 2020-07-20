9666b5efb1400b422c237665a29b2ab6699426dc
Leo <leoaee@gmail.com>
2020-07-20 22:25:46 +0800

    Update template.js

--- template.js
+++ template.js
@@ -24,6 +24,6 @@ export default `> 👨🏻‍💻 Web Front-end Developer
   </pre>
 </details>
 
-![Build README](https://github.com/mopig/mopig/workflows/build/badge.svg)
+![build](https://github.com/mopig/mopig/workflows/build/badge.svg)
 ![GitHub last commit](https://img.shields.io/github/last-commit/mopig/mopig)
 ![pv](https://pageview.vercel.app/?github_user=mopig)`
